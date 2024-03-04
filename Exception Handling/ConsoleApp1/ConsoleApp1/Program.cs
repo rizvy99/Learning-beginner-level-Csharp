977e8636a670747556ddac5b309fed305829ca7f
Alrazi Hosen Rizvy <ahrizvy78@gmail.com>
2024-03-04 12:48:01 +0600

    Exception Handling

---
+++ Exception Handling/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number : ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter another number : ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Your answer is : " + num1 / num2);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            /*catch (DivideByZeroException b)
+            {
+                Console.WriteLine(b.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }*/
+
+            Console.ReadLine();
+        }
+    }
+}
