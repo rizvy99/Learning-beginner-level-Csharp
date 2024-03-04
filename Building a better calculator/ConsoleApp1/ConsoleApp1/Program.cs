29e382c5225e67a1200a3acb66dbe32ad66a4943
Alrazi Hosen Rizvy <ahrizvy78@gmail.com>
2024-03-04 11:31:46 +0600

    Building a simple calculator

---
+++ Building a better calculator/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ans;
+            Console.Write("Enter a number : ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter an operation : ");
+            string op = Console.ReadLine();
+            Console.Write("Enter another number : ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            if (op == "+")
+            {
+                ans = num1 + num2;
+                Console.WriteLine("Your answer is : " + ans);
+            }
+            else if(op == "-")
+            {
+                ans = num1 - num2;
+                Console.WriteLine("Your answer is : " + ans);
+            }
+            else if(op == "*")
+            {
+                ans = num1 * num2;
+                Console.WriteLine("Your answer is : " + ans);
+            }
+            else if(op == "/")
+            {
+                ans = num1 / num2;
+                Console.WriteLine("Your answer is : " + ans);
+            }
+            else
+            {
+                Console.WriteLine("********Invalid number!!***********");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
